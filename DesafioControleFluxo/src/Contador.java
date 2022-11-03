2f389771467424357ab7c3c40baf0be2cd4e87ef
VitorEmerique <vitort.emerique@hotmail.com>
2022-11-03 19:29:22 -0300

    push

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException Exception) {
+			System.err.println(Exception);
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		
+			if(parametroUm>parametroDois) {
+				throw new ParametrosInvalidosException();
+			}else {
+				
+			
+		
+				int contagem = parametroDois - parametroUm;
+				for(int i = 1; i<=contagem;i++) {
+					System.out.println("Imprimindo "+ i+"\n");
+				}
+		
+	}
+
+	}
+
+}
